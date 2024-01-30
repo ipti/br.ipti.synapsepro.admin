15e1f85995844d6fbceb63f75e146df318907801
jonny paulino <jonnyzico10@gmail.com>
2024-01-30 09:33:57 -0300

    fix: components

--- src/Components/ComponentCheckbox/View/index.tsx
+++ src/Components/ComponentCheckbox/View/index.tsx
@@ -1,25 +1,44 @@
-
 import CheckboxComponent from "../../Checkbox";
 import { Column } from "../../../Styles/styles";
 import { PropsCheckBoxCardView } from "../../../Types/types";
+import { ControllerViewForm } from "../../../Controller/controllerViewForm";
 
-export default function RenderCheckBoxCard({ options, handleChange, item }: PropsCheckBoxCardView) {
-
-    return (
-        <div className="card flex justify-content-start">
-            <div className="flex flex-column gap-3">
-                {options?.map((category) => {
-                    return (
-                        <div key={category.key} className="flex align-items-start">
-                            <Column id="center">
-                                <CheckboxComponent checked={category.value} onChange={(e) => {handleChange!(e.checked, item.id, category.id)}} />
-                            </Column>                            <Column id="center">
-                                <label htmlFor={category.key} className="ml-2">{category.label}</label>
-                            </Column>
-                        </div>
+export default function RenderCheckBoxCard({
+  options,
+  item,
+  form,
+  setform,
+}: PropsCheckBoxCardView) {
+  const props = ControllerViewForm();
+  return (
+    <div className="card flex justify-content-start">
+      <div className="flex flex-column gap-3">
+        {options?.map((category) => {
+          return (
+            <div key={category.key} className="flex align-items-start">
+              <Column id="center">
+                <CheckboxComponent
+                  checked={category.value}
+                  onChange={(e) => {
+                    props.RespQuestionCheckBox!(
+                      e.checked,
+                      item.id,
+                      category.id,
+                      form,
+                      setform
                     );
-                })}
+                  }}
+                />
+              </Column>{" "}
+              <Column id="center">
+                <label htmlFor={category.key} className="ml-2">
+                  {category.label}
+                </label>
+              </Column>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/Pages/ViewForms/index.tsx
+++ src/Pages/ViewForms/index.tsx
@@ -1,30 +1,23 @@
+import { Form, Formik } from "formik";
+import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { useContext, useEffect, useState } from "react";
-import TextAreaComponent from "../../Components/TextArea";
-import TextInput from "../../Components/TextInput";
-import { AplicationContext } from "../../Context/Aplication/context";
-import { Column, Container, Padding, Row } from "../../Styles/styles";
-import { PropsAplicationContext } from "../../Types/types";
-import CheckBoxCard from "../../Components/ComponentCheckbox/View";
-import RadioButtonCard from "../../Components/ComponentMulti/View";
-import { Button } from "primereact/button";
-import { Form, Formik } from "formik";
 import RenderCheckBoxCard from "../../Components/ComponentCheckbox/View";
 import RenderRadioButtonCard from "../../Components/ComponentMulti/View";
 import RenderViewTextField from "../../Components/ComponentTextFiled/View";
+import RenderViewTextLong from "../../Components/ComponentTextLong/View";
+import { AplicationContext } from "../../Context/Aplication/context";
+import { Column, Container, Padding, Row } from "../../Styles/styles";
+import { PropsAplicationContext } from "../../Types/types";
 
 const ViewForms = () => {
   const { form } = useContext(AplicationContext) as PropsAplicationContext;
 
   const [formRespo, setFormResp] = useState(form ?? []);
 
-
   useEffect(() => {
-    setFormResp(form)
-  }, [form])
-
-
-
+    setFormResp(form);
+  }, [form]);
 
   console.log(formRespo);
 
@@ -43,56 +36,71 @@ const ViewForms = () => {
           </Padding>
         </Card>
       </Padding>
-      <Formik initialValues={{}} onSubmit={(values) => { }}>
+      <Formik initialValues={{}} onSubmit={(values) => {}}>
         {({ values, setFieldValue }) => {
           return (
             <Form>
               {formRespo?.question.map((item, index) => {
                 return (
                   <Padding padding="4px" key={index}>
-
                     <Card>
-                      <Row id="end">
-                        
-                      </Row>
+                      <Row id="end"></Row>
                       <Padding padding="16px">
                         <Row id="space-between">
                           <Column id="center">
                             <p>{item.label}</p>
                           </Column>
-                         {item.required ?  <Column id="start"><h3 style={{ color: "red" }}>*</h3></Column> : null}
+                          {item.required ? (
+                            <Column id="start">
+                              <h3 style={{ color: "red" }}>*</h3>
+                            </Column>
+                          ) : null}
                         </Row>
                       </Padding>
-                      {
-                        item?.type === "text" ? (
-                          <RenderViewTextField form={form} setFormResp={setFormResp} item={item} />
-                        ) : item?.type === "text-long" ? (
-                          <Padding padding="16px">
-                            {/* <TextAreaComponent placeholder="Resposta longa" value={item?.value} onChange={(e) => RespQuestion(e.target.value, item.id)} /> */}
-                          </Padding>
-                        ) : item?.type === "mult" ? (
-                          <div>
-                            <RenderRadioButtonCard options={item.options} item={item} form={formRespo} setFormResp={setFormResp} />
-                          </div>
-                        ) : item?.type === "select-box" ? (
-                          <div>
-                            <RenderCheckBoxCard options={item?.options} item={item} form={formRespo} setform={setFormResp} />
-                          </div>
-                        ) : null
-                      }
+                      {item?.type === "textfield" ? (
+                        <RenderViewTextField
+                          form={form}
+                          setFormResp={setFormResp}
+                          item={item}
+                        />
+                      ) : item?.type === "textlong" ? (
+                        <RenderViewTextLong
+                          form={form}
+                          setFormResp={setFormResp}
+                          item={item}
+                        />
+                      ) : item?.type === "mult" ? (
+                        <div>
+                          <RenderRadioButtonCard
+                            options={item.options}
+                            item={item}
+                            form={formRespo}
+                            setFormResp={setFormResp}
+                          />
+                        </div>
+                      ) : item?.type === "checklist" ? (
+                        <div>
+                          <RenderCheckBoxCard
+                            options={item?.options}
+                            item={item}
+                            form={formRespo}
+                            setform={setFormResp}
+                          />
+                        </div>
+                      ) : null}
                     </Card>
-                  </Padding >
+                  </Padding>
                 );
               })}
               <Padding padding="8px" />
               <Row id="end">
                 <Button icon="pi pi-send" label="Enviar" />
               </Row>
-            </Form >
+            </Form>
           );
         }}
-      </Formik >
-    </Container >
+      </Formik>
+    </Container>
   );
 };
 
