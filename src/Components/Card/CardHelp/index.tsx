bbddd48ebb2e90b8f5fe61c6c6c60097550309fa
TI JONNY <ti.jonny@ipti.org.br>
2024-06-18 16:57:16 -0300

    initial page help

---
+++ src/Components/Card/CardHelp/index.tsx
@@ -0,0 +1,9 @@
+const CardHelp = () => {
+    return (
+        <div className="card">
+            djkdf
+        </div>
+    )
+}
+
+export default CardHelp;
--- src/Pages/Help/index.tsx
+++ src/Pages/Help/index.tsx
@@ -1,9 +1,16 @@
-import { Container } from "../../Styles/styles";
+import CardHelp from "../../Components/Card/CardHelp";
+import { Container, Padding } from "../../Styles/styles";
 
 const Help = () => {
     return(
         <Container>
-            
+            <h1>Ajuda</h1>
+            <Padding padding="16px" />
+            <div className="grid">
+                <div className="col-12 md:col-6">
+                    <CardHelp  />
+                </div>
+            </div>
         </Container>
     )
 }
